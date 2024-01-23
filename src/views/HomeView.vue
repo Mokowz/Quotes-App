25cd61e744e7cd501e3ad9a3a3df061ce583deed
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-23 14:09:18 +0300

    Fetch random quotes first

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,48 @@
 <template>
-  <h1 class="text-3xl font-medium">Hey</h1>
+  <div class="bg-blue-400 h-screen">
+    <!-- FLex Container -->
+    <div class="flex mx-auto container flex-col py-10 px-6 md:px-0">
+      <!-- Nav -->
+
+      <!-- Quote Container -->
+      <h1 class="text-3xl font-medium" v-for="quote in quotes" :key="quote.quote">{{ quote.quote }}</h1>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   name: 'HomeView',
-  components: { }
+  components: { },
+  data() {
+    return {
+      quotes: null,
+      category: null
+    }
+  },
+  beforeMount() {
+    this.fetchRandomQuote()
+  },
+  methods: {
+    async fetchRandomQuote() {
+      const API_KEY = 'MB1uM1dT9DRh09Syv29+3g==HdhrmcROxUekrBRY'
+
+      try {
+        const response = await axios.get(`https://api.api-ninjas.com/v1/quotes/`,
+        {
+          headers: {
+            'X-Api-Key': API_KEY
+          },
+        });
+
+        this.quotes = response.data
+        console.log(response.data);
+      } catch(error) {
+        console.log(error)
+      }
+    }
+  }
 }
 </script>
